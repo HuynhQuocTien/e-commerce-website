141b310f6f243ebe8981d4c75995f4c73085e49f
Huynh Quoc Tien <quoctien01062003@gmail.com>
2024-11-10 10:16:09 +0700

    up helper:  dto request

---
+++ Helper/Product/GetProductPagingRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace e_commerce_website.Helper.Product
+{
+    public class GetProductPagingRequest : PagingRequestBase
+    {
+        public string keyword { get; set; }
+        public int? categoryId { get; set; }
+    }
+}
--- obj/Debug/net8.0/e-commerce-website.AssemblyInfo.cs
+++ obj/Debug/net8.0/e-commerce-website.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("e-commerce-website")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+43785eaf037dc90ace8da003eaebfe9244180538")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+33b9d7428d5392c072f5efb182af6e7be2c436b4")]
 [assembly: System.Reflection.AssemblyProductAttribute("e-commerce-website")]
 [assembly: System.Reflection.AssemblyTitleAttribute("e-commerce-website")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
